4da72020f19612552b5444c18ff08672a9632ad6
Jeff Talada <j.talada@bellevuecollege.edu>
2019-08-04 02:39:38 -0700

    Final

--- src/Client.java
+++ src/Client.java
@@ -1,31 +1,33 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
+import java.util.Scanner;
 
 public class Client {
 
-//	                       {0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5}
+//		   eliminated      {1,7,9,11,13,14}
+//	       attribute % 10  {0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5}
 	static int options[] = {4,4,5,6,3,5,5,4,4,3,3,3,4,4,2,2};
 	static String labels[] = {"checking-balance", "months-loan-duration", "credit-history", "purpose", "amount", "savings-balance", "employment-duration", "percent-of-income", "years-at-residence", "age", "other-credit", "housing", "existing-loans-count", "job", "dependents", "default"};
 
 	static Node root = new Node(15, 0, null);
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-		Data data = new Data(new File("credit.csv"));
+		Data data = new Data(new File("credit.csv"), false, true, 1000);
 		
-		Boolean testSelection[] = new Boolean[1000];
-		int[][] trainingSet = new int[900][16];
-		int[][] testSet = new int[100][16];
-		double averageAccuracy = 0.0;
+		Boolean testSelection[];
+		int[][] trainingSet;
+		int[][] testSet;
+//		double averageError = 0.0;
 
-
-		for (int repeat = 0; repeat < 30; repeat++) {
-			
-			for (int i = 0; i < 1000; i++) {testSelection[i] = false;}
-			randomSelection(testSelection, 100, 1000);
+//		for (int repeat = 0; repeat < 10000; repeat++) {
+			root = new Node(15, 0, null);
+			testSelection = randomSelection(data, 100, 1000);
+			trainingSet = new int[900][16];
+			testSet = new int[100][16];
 			
 			int j=0, k=0;
 			for (int i = 0; i < 1000; i++) {
@@ -37,45 +39,43 @@ public class Client {
 					k++;
 				}
 			}
-	
-	// 1st level gains
-	//		
-	//		for (int i = 0; i < 15; i++) {
-	//			double gain = gain(data.getData(), i, 15);
-	//			System.out.println(i + " " + gain);
-	//		}
-			
-	//		entropyAttributes(data.getData(), 7, 15);
+			Data trainingCases = new Data(trainingSet);
+			trainingCases.writeData(new File("training_data.csv"));
+			Data testCases = new Data(testSet);
+			testCases.writeData(new File("test_data.csv"));
 			
 			train(trainingSet);
-			double hit = 0.0;
+			double miss = 0.0;
 			for (int i = 0; i < 100; i++) {
 				Boolean result = test(root, testSet[i]);
-				if ((result == false && testSet[i][15] == 0) || (result == true && testSet[i][15] == 1)) {hit++;}
-	//			System.out.println(result + " : " + testSet[i][15]);
+				if (!(result == false && testSet[i][15] == 0) && !(result == true && testSet[i][15] == 1)) 
+					miss++;
 			}
-//			System.out.println(hit + "%");
-			averageAccuracy += hit;
+			System.out.println("This model's error rate: " + miss + "%\n");
 			
-	//		printTree(root);
+			System.out.print("Enter the name of your test cases file: ");
+			Scanner in = new Scanner(System.in);
+			String userFile = in.nextLine();
+			System.out.print("Enter number of test cases: ");
+			int cases = in.nextInt();
+			in.close();
+			Data userTests = new Data(new File(userFile), false, false, cases);
+			int[][] newTests = userTests.getData();
+			
+			miss = 0.0;
+			for (int i = 0; i < cases; i++) {
+				Boolean result = test(root, newTests[i]);
+				if (!(result == false && newTests[i][15] == 0) && !(result == true && newTests[i][15] == 1)) 
+					miss++;
+			}
 
-			
+			System.out.println("The user's test case error rate: " + miss/cases*100 + "%\n");
+
+//			averageError += miss;
 		}
-		System.out.println(averageAccuracy/30 + "%");
-	}
+//		System.out.println("Average Error: " + averageError/10000.0 + "%");
+//	}
 	
-	static void printTree(Node root) {
-		if (root.getChildren() == null) {
-			System.out.println("null children: " + root.getResult());
-		} else {
-			System.out.println("(\r\n" + root.getAttribute() + ", " + root.getCategory() + ", " + root.getResult());
-			for (int i = 0; i < root.getChildren().size(); i++) {
-				printTree(root.getChildren().get(i));
-			}
-			System.out.println(")");
-		}
-	}
-
 	static Boolean test(Node decisionTree, int[] testCase) {
 		Node current = decisionTree;
 		while (current.getChildren().size() > 1) {
@@ -85,81 +85,88 @@ public class Client {
 		return current.getChildren().get(0).getResult();
 	}
 
-	static void train(int[][] trainingSet) throws InterruptedException {
+	// Builds a decision tree
+	static void train(int[][] trainingSet) {
 		List<Boolean> traversed = new ArrayList<Boolean>();
 		for (int i = 0; i < 15; i++) {traversed.add(false);}
+
+		// Eliminated attributes
+		traversed.set(1, true);
+		traversed.set(7, true);
+		traversed.set(9, true);
+		traversed.set(11, true);
+		traversed.set(13, true);
+		traversed.set(14, true);
+		
 		train(root, trainingSet, 15, traversed);
 	}
 	
-	static Node train(Node root, int[][] trainingSet, int targetAttribute, List<Boolean> traversedAttributes) throws InterruptedException {
-//		System.out.print("[" + labels[root.getAttribute()] + "_" + root.getCategory() + " ");
+	// Recursively builds the decision tree
+	static Node train(Node root, int[][] trainingSet, int targetAttribute, List<Boolean> traversedAttributes) {
 		int examples = trainingSet.length;
 		int positives = 0;
 		for (int i = 0; i < examples; i++) {
 			if (trainingSet[i][15] == 1)
 				positives++;
 		}
-		if (positives == examples || (examples == 2 && positives == 1)) {
-			root.addNode(null, null, true);
-//			System.out.println("[" + root.getChildren().get(0).getResult() + "]]");
-			return root;
-		} else if (positives == 0) {
+		
+		// leaf node if all results are no or there is a tie with 2 left
+		if (positives == 0 || (examples == 2 && positives == 1)) {
 			root.addNode(null, null, false);
-//			System.out.println("[" + root.getChildren().get(0).getResult() + "]]");
 			return root;
-		} else if (!traversedAttributes.contains(false) || examples < 60) {
+			
+		// leaf node if all results are yes
+		} else if (positives == examples) {
+			root.addNode(null, null, true);
+			return root;
+			
+		// leaf node if all attributes have been used
+		} else if (!traversedAttributes.contains(false)) {
 			if (positives > examples) {
 				root.addNode(null, null, true);
-//				System.out.println("[" + root.getChildren().get(0).getResult() + "]]");
 				return root;
 			} else {
 				root.addNode(null, null, false);
-//				System.out.println("[" + root.getChildren().get(0).getResult() + "]]");
 				return root;
 			}
 		} else {
+			
+			// find attribute with highest gain
 			int indexMax = 0;
-			double maxEntropy = 0.0, anteMaxEntropy = 0.0, temp = 0.0;
+			double maxGain = 0.0, temp = 0.0;
 			for (int i = 0; i < 15; i++) {
 				if (!traversedAttributes.get(i)) {
 					temp = gain(trainingSet, i, 15);
-					if (temp > maxEntropy) {
+					if (temp > maxGain) {
 						indexMax = i;
-//						if (maxEntropy > 0.0) {
-							anteMaxEntropy = maxEntropy;
-//						}
-						maxEntropy = temp;
+						maxGain = temp;
 					}
 				}
 			}
-//			System.out.println(anteMaxEntropy + " " + maxEntropy + " " + (anteMaxEntropy / maxEntropy));
-//			if (anteMaxEntropy > 0.0 && (anteMaxEntropy / maxEntropy) <= 0.0125) {
-//				if (positives > examples) {
-//					root.addNode(null, null, true);
-//					System.out.println("[" + root.getChildren().get(0).getResult() + "]]");
-//					return root;
-//				} else {
-//					root.addNode(null, null, false);
-//					System.out.println("[" + root.getChildren().get(0).getResult() + "]]");
-//					return root;
-//				}
-//			}
-//			if (root.getAttribute() == 0) {
-//				System.out.println("here?");
-//			}
+			
+			// leaf node if gain is too low
+			if (maxGain < 0.04) {
+				if (positives > examples) {
+					root.addNode(null, null, true);
+					return root;
+				} else {
+					root.addNode(null, null, false);
+					return root;
+				}
+			}
+			
+			// root node
 			traversedAttributes.set(indexMax, true);
-//			System.out.println(traversedAttributes.toString());
-//			TimeUnit.MILLISECONDS.sleep(100);
 			for (int i = 0; i < options[indexMax]; i++) {
 				int[][] subset = getSubSet(trainingSet, indexMax, i);
 				root.addNode(train(new Node(indexMax, i, null), subset, indexMax, traversedAttributes));
 			}
 			traversedAttributes.set(indexMax, false);
-//			System.out.println("]");
 			return root;
 		}
 	}
 	
+	// returns a subset containing a category from the given set
 	static int[][] getSubSet(int[][] set, int attribute, int category) {
 		int size = 0;
 		for (int i = 0; i < set.length; i++) {
@@ -176,10 +183,12 @@ public class Client {
 		return subSet;
 	}
 	
+	// finds the gain of an attribute
 	static double gain(int[][] set, int attribute, int resultCol) {
 		return entropySet(set, resultCol) - entropyAttributes(set, attribute, resultCol);
 	}
 	
+	// finds the entropy of the given set
 	static double entropySet(int[][] set, int attribute) {
 		double examples = set.length, positives = 0.0;
 		for (int i = 0; i < examples; i++) {
@@ -188,16 +197,15 @@ public class Client {
 			}
 		}
 		double entropy = -(positives/examples) * (Math.log(positives/examples)/Math.log(2)) - ((examples-positives)/examples) * (Math.log((examples-positives)/examples)/Math.log(2));
-//		System.out.println(positives + "\t" + examples + "\t" + entropy);
 		return entropy;
 	}
 	
+	// finds the entropy of an attribute in the given set
 	static double entropyAttributes(int[][] set, int attribute, int resultCol) {
 		double entropy = 0.0;
 		double categoryCount = 0.0, positives = 0.0;
 		int examples = set.length;
 		for (int category = 0; category < options[attribute]; category++) {
-//			System.out.println(category);
 			for (int i = 0; i < examples; i++) {
 				if (set[i][attribute] == category) {
 					categoryCount++;
@@ -208,7 +216,6 @@ public class Client {
 			}
 			if (examples != 0 && categoryCount != 0 && positives != 0 && positives != categoryCount) {
 				entropy += (categoryCount/examples) * (-(positives/categoryCount) * (Math.log(positives/categoryCount)/Math.log(2.0)) - ((categoryCount-positives)/categoryCount) * (Math.log((categoryCount-positives)/categoryCount)/Math.log(2.0))); 
-//				System.out.println(categoryCount + "\t" + examples + "\t" + positives + "\t" + entropy);
 			}
 			categoryCount = 0.0;
 			positives = 0.0;
@@ -216,18 +223,30 @@ public class Client {
 		return entropy;
 	}
 	
-	static void randomSelection(Boolean[] a, int select, int size) {
+	// Select test cases while maintaining the ratio of yeses to noes
+	static Boolean[] randomSelection(Data data, int select, int size) {
 		Random r = new Random();
-		
+		Boolean[] selection = new Boolean[1000];
+		Arrays.fill(selection, false);
+
+		int neg=70, pos=30;
 		while (select > 0) {
 			int i = r.nextInt(size - 1);
-			if (!a[i]) {
-				a[i] = true;
-				select--;
+			if (!selection[i]) {
+				if ((data.getData()[i][15] == 0) && neg > 0) {
+					neg--;
+					selection[i] = true;
+					select--;
+				}
+				if ((data.getData()[i][15] == 1) && pos > 0) {
+					pos--;
+					selection[i] = true;
+					select--;
+				}
 			}
 		}
+		return selection;
 	}
-
 }
 
 
